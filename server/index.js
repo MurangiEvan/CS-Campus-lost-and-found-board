f67a69feba54dd9083cc0666feda892b98559479
MurangiEvan <mashau.ivan@gmail.com>
2026-09-15 15:04:52 +0200

    server

--- server/index.js
+++ server/index.js
@@ -3,6 +3,10 @@ const process = require('node:process');
 
 const PORT = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+  res.send('Status: ok!');
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
